d2f1b52ecc8f6d38417dcea6c3618a99be010c1b
Richard Yvarra <richard.yvarra@gmail.com>
2021-04-27 19:46:56 -0400

    models script

--- models/index.js
+++ models/index.js
@@ -5,12 +5,27 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'Category_id',
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'Category_id',
+
+
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Product, {
+  through: 'Product_id',
+
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  foreignKey: 'Tag_id',
+});
 
 module.exports = {
   Product,
